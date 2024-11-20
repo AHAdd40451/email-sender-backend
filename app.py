6487c270d311fd6d5ee12c2f778db14406261116
AHAdd40451 <ahadkhan40451@gmail.com>
2024-11-20 22:02:10 +0500

    fix cinlifclt

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 # Pass socketio instance to bulk_email module
 bulk_email.socketio = socketio
@@ -110,8 +110,4 @@ def clear_logs():
         }), 500
 
 if __name__ == '__main__':
-    socketio.run(app, 
-                 host='0.0.0.0',
-                 port=5000,
-                 debug=False,
-                 allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, port=5000, host='0.0.0.0')
